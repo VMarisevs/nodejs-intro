c38319eda88d3c0826537dbfb304fd2ed944f331
Vladislavs Marisevs <vmarisevs@gmail.com>
2015-10-08 12:59:28 +0100

    Did 1-6 exercises + 1,2 advanced

--- telnet.js
+++ telnet.js
@@ -6,7 +6,12 @@ var net = require('net');
  * Callback method executed when a new TCP socket is opened.
  */
 function newSocket(socket) {
-	socket.write('Welcome to the Telnet server!');
+	//socket.write('Welcome to the Telnet server!');
+	
+		setTimeout(function() {
+				socket.write('Hello World');		
+			}, 3000);
+	
 }
  
 // Create a new server and provide a callback for when a connection occurs
